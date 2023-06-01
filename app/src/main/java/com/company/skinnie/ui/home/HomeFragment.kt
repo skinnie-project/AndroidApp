a4a84411b331bf7a3dc95a4ffbf9df666d39f444
putriaulia23 <putriromadhon42@gmail.com>
2023-06-01 14:50:02 +0800

    merge project and add scan feature

--- app/src/main/java/com/company/skinnie/ui/home/HomeFragment.kt
+++ app/src/main/java/com/company/skinnie/ui/home/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.company.skinnie.ui.ui.home
+package com.company.skinnie.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
--- app/src/main/java/com/company/skinnie/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/company/skinnie/ui/home/HomeViewModel.kt
@@ -1,4 +1,4 @@
-package com.company.skinnie.ui.ui.home
+package com.company.skinnie.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
